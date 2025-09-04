a37d89fb51d8c55d0bda3f3f3eace59322b60179
Abdullah Ahmed <abdol-ahmed@users.noreply.github.com>
2025-09-03 23:19:22 -0700

    Code clean up

--- command_help.go
+++ command_help.go
@@ -5,13 +5,10 @@ import (
 )
 
 func commandHelp() error {
-	fmt.Println()
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
-	fmt.Println()
 	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		fmt.Printf("\t%s: %s\n", cmd.name, cmd.description)
 	}
-	fmt.Println()
 	return nil
 }
