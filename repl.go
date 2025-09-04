2c941444c17e934a5f8f6eea6285fae606017743
Abdullah Ahmed <abdol-ahmed@users.noreply.github.com>
2025-09-03 23:13:30 -0700

    Add command support for "Help and Exit"

--- repl.go
+++ repl.go
@@ -10,19 +10,50 @@ import (
 func StartREPL() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		words := cleanInput(scanner.Text())
 		if len(words) == 0 {
 			continue
 		}
 		command := words[0]
+		executeCommand(command)
+	}
+}
 
-		fmt.Printf("Your command was: %s\n", command)
+func executeCommand(commandName string) {
+	if cmd, ok := getCommands()[commandName]; ok {
+		err := cmd.callback()
+		if err != nil {
+			err2 := fmt.Errorf("%w", err)
+			fmt.Println(err2.Error())
+		}
+	} else {
+		fmt.Printf("Unknown command\n")
 	}
 }
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	return strings.Fields(output)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
